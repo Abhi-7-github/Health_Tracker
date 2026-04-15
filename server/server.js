1584eceb9b5b3ef2eb76136589e795e9edc92970
Abhi-7-github <abhikollepara333@gmail.com>
2026-04-15 08:42:17 +0530

    latest

--- server/server.js
+++ server/server.js
@@ -14,5 +14,5 @@ require("mongoose").connect(MONGO_URI)
 
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running at http://localhost:${PORT}`);
 });
